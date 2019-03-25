075911adcb37d9e8baaab9d620aa465c23d2eece
Annabelle Taylor <annabelle.t.taylor@gmail.com>
2019-03-25 13:30:04 -0400

    Init commit

---
+++ src/index.js
@@ -0,0 +1,7 @@
+const { GraphQLServer } = require("graphql-yoga");
+
+const server = new GraphQLServer({
+	typeDefs: "./src/schema.graphql",
+	resolvers
+});
+server.start(() => console.log(`Server is running on http://localhost:4000`));
